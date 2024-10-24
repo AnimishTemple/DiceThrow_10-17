cf5799a31f13644183a827f7ec014cce18fe6792
justanimish <justanimish@proton.me>
2024-10-24 11:39:58 -0400

    Final version before view models are implemented

--- app/src/main/java/edu/temple/dicethrow/ButtonFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/ButtonFragment.kt
@@ -19,14 +19,17 @@ class ButtonFragment : Fragment() {
     }
 
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_button, container, false).apply {
-            findViewById<Button>(R.id.button).setOnClickListener {
-                (requireActivity() as ButtonInterface).buttonClicked()
-            }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?) : View? {
+
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_button, container, false).apply {
+                findViewById<Button>(R.id.button).setOnClickListener {
+                    (requireActivity() as ButtonInterface).buttonClicked()
+                }
+
         }
     }
 
