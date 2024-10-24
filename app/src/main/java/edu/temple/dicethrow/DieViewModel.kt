cc0c678dee59bdc88026b739c5f7a7e99bbc5f60
justanimish <justanimish@proton.me>
2024-10-24 11:54:10 -0400

    Created setter and getter functions for ViewModel

--- app/src/main/java/edu/temple/dicethrow/DieViewModel.kt
+++ app/src/main/java/edu/temple/dicethrow/DieViewModel.kt
@@ -1,11 +1,20 @@
 package edu.temple.dicethrow
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DieViewModel : ViewModel {
+class DieViewModel : ViewModel() {
 
     // Might need to change this
-    private val dieRoll : MutableLiveData<Int> = MutableLiveData<Int>()
+    private val dieRoll: MutableLiveData<Int> = MutableLiveData<Int>()
+
+    fun getDieRoll() : LiveData<Int> {
+        return dieRoll
+    }
+
+    fun setDieRoll(newRoll: Int) {
+        dieRoll.value = newRoll
+    }
 
 }
